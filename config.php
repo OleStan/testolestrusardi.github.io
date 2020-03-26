04d8849433a8f2fb6b5c4465212b2f0cc60e9248
OleStan <59766354+OleStan@users.noreply.github.com>
2020-03-26 12:40:52 +0200

    origin
    1

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/teststore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/teststore/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/XAMPP/htdocs/teststore/catalog/');
+define('DIR_SYSTEM', 'D:/XAMPP/htdocs/teststore/system/');
+define('DIR_IMAGE', 'D:/XAMPP/htdocs/teststore/image/');
+define('DIR_LANGUAGE', 'D:/XAMPP/htdocs/teststore/catalog/language/');
+define('DIR_TEMPLATE', 'D:/XAMPP/htdocs/teststore/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/XAMPP/htdocs/teststore/system/config/');
+define('DIR_CACHE', 'D:/XAMPP/htdocs/teststore/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/XAMPP/htdocs/teststore/system/storage/download/');
+define('DIR_LOGS', 'D:/XAMPP/htdocs/teststore/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/XAMPP/htdocs/teststore/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/XAMPP/htdocs/teststore/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'teststore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
