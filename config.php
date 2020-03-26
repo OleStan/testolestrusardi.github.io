4a9b36bedeb309cc215f0fea6472bec924a37f74
OleStan <59766354+OleStan@users.noreply.github.com>
2020-03-26 12:44:12 +0200

    v2
    1

--- config.php
+++ config.php
@@ -1,22 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/teststore/');
+define('HTTP_SERVER', 'http://localhost/teststore/admin/');
+define('HTTP_CATALOG', 'http://localhost/teststore/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/teststore/');
+define('HTTPS_SERVER', 'http://localhost/teststore/admin/');
+define('HTTPS_CATALOG', 'http://localhost/teststore/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/XAMPP/htdocs/teststore/catalog/');
+define('DIR_APPLICATION', 'D:/XAMPP/htdocs/teststore/admin/');
 define('DIR_SYSTEM', 'D:/XAMPP/htdocs/teststore/system/');
 define('DIR_IMAGE', 'D:/XAMPP/htdocs/teststore/image/');
-define('DIR_LANGUAGE', 'D:/XAMPP/htdocs/teststore/catalog/language/');
-define('DIR_TEMPLATE', 'D:/XAMPP/htdocs/teststore/catalog/view/theme/');
+define('DIR_LANGUAGE', 'D:/XAMPP/htdocs/teststore/admin/language/');
+define('DIR_TEMPLATE', 'D:/XAMPP/htdocs/teststore/admin/view/template/');
 define('DIR_CONFIG', 'D:/XAMPP/htdocs/teststore/system/config/');
 define('DIR_CACHE', 'D:/XAMPP/htdocs/teststore/system/storage/cache/');
 define('DIR_DOWNLOAD', 'D:/XAMPP/htdocs/teststore/system/storage/download/');
 define('DIR_LOGS', 'D:/XAMPP/htdocs/teststore/system/storage/logs/');
 define('DIR_MODIFICATION', 'D:/XAMPP/htdocs/teststore/system/storage/modification/');
 define('DIR_UPLOAD', 'D:/XAMPP/htdocs/teststore/system/storage/upload/');
+define('DIR_CATALOG', 'D:/XAMPP/htdocs/teststore/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
